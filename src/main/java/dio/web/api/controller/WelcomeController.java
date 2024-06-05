a877fd7a74f46d8f3da3a69552fea6b01f990540
Ramon-denv <ramonmendespeixoto008@gmail.com>
2024-06-05 16:14:40 -0300

    criação api dio

--- src/main/java/dio/web/api/controller/WelcomeController.java
+++ src/main/java/dio/web/api/controller/WelcomeController.java
@@ -1,13 +1,38 @@
 package dio.web.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dio.web.api.repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class WelcomeController {
+@RequestMapping("/users")
+public class UserController {
 
+    @Autowired
+    private UserRepository repository;
     @GetMapping
-    public String welcome(){
-        return "Welcolme to My Spring Boot Web API";
+    public List<User> list(){
+        return repository.findAll();
     }
+    @PostMapping
+    public void save(@RequestBody User user){
+        repository.save(user);
+    }
+    @PutMapping
+    public void update(@RequestBody User user){
+        repository.save(user);
+    }
+    @GetMapping("/{username}")
+    public User find(@PathVariable("/username") String username){
+        return repository.findByUsername(username);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("/id") Integer id){
+        repository.deleteById(id);
+    }
+}
+
 }
